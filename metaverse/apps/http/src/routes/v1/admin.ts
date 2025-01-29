6ba6afa8ce892f3ebf1a7c6b4844e935213098b4
Prasad Gurude <prasadgurude76@gmail.com>
2025-01-29 22:55:16 +0530

    basic routes skelitan is done in http backed logic

---
+++ metaverse/apps/http/src/routes/v1/admin.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+export const adminRouter = Router();
+
+adminRouter.post('/element', (Req, res) => {
+
+})
+adminRouter.put('/element/:elementId', (Req, res) => {
+
+})
+adminRouter.post('/avatar', (Req, res) => {
+
+})
+adminRouter.get('/map', (Req, res) => {
+
+})
--- metaverse/apps/http/src/routes/v1/index.ts
+++ metaverse/apps/http/src/routes/v1/index.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { userRouter } from "./user";
+import { spaceRouter } from "./space";
+import { adminRouter } from "./admin";
 
 export const router = Router();
-
-router.get("/", (req, res) => {
-    res.json({ message: "Welcome to API v1" });
-});
-
+router.use("/user",userRouter)
+router.use("/space",spaceRouter)
+router.use("/admin",adminRouter)
 
 router.post("/signup", (req, res) => {
     res.json({ message: "User signed up" });
