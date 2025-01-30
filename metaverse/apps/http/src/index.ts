204bf488fd85273043e9a771332a0804a4a50d90
Prasad Gurude <prasadgurude76@gmail.com>
2025-01-30 13:56:39 +0530

    bd is setpu in and imported in http server

--- metaverse/apps/http/src/index.ts
+++ metaverse/apps/http/src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import { router } from "./router/v1";
+import client from "@repo/db"
 
 const app = express();
 
-app.use("/api/v1",router)
-app.get('/',(req,res)=>{
+app.use("/api/v1", router)
+ 
+app.get('/', (req, res) => {
     res.send("hello world")
 })
 
-app.listen(process.env.PORT || 3000,()=>{
+app.listen(process.env.PORT || 3000, () => {
     console.log("server is up an drunning")
 })
