d08aa7038e2d8d12d2aa4bf0dbe5f3539a446ebd
Prasad Gurude <prasadgurude76@gmail.com>
2025-01-31 14:02:36 +0530

    sitnup and signin are working but the tests are failing 2 tests are failing for signup and signin out of 4

--- metaverse/apps/http/src/index.ts
+++ metaverse/apps/http/src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { router } from "./router/v1";
-import client from "@repo/db"
+import client from "@repo/db/client"
+// const client = require("@repo/db/client");
 
 const app = express();
 
+app.use(express.json())
 app.use("/api/v1", router)
  
 app.get('/', (req, res) => {
@@ -11,5 +13,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("server is up an drunning")
+    console.log("server is up an running")
 })
--- metaverse/packages/db/src/index.ts
+++ metaverse/packages/db/src/index.ts
@@ -1,3 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-
 export default new PrismaClient();
+// const { PrismaClient } = require("@prisma/client");
+// const client = require("@repo/db/client");
+
