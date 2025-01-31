9a6cf1968bd6a3f5222a6155b8d1b178ddc59c88
Prasad Gurude <prasadgurude76@gmail.com>
2025-01-31 22:08:17 +0530

    the tests where failin because of the zod schem validation

--- metaverse/apps/http/src/router/v1/index.ts
+++ metaverse/apps/http/src/router/v1/index.ts
@@ -1,20 +1,14 @@
-import { Router } from 'express'
-import { userRouter } from './user';
-import { adminRouter } from './admin';
-import { spaceRouter } from './space';
-import { SigninSchema, SignupSchema } from '../../types';
+import { Router } from "express";
+import { userRouter } from "./user";
+import { spaceRouter } from "./space";
+import { adminRouter } from "./admin";
+import { SigninSchema, SignupSchema } from "../../types";
 import {hash, compare} from "../../scrypt";
+import client from "@repo/db/client";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../../config";
 
-// import bcrypt from "bcrypt"
-import client from "@repo/db/client"
-// const client = require("@repo/db/client");
-import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../../config';
 export const router = Router();
-
-router.use("/user", userRouter)
-router.use("/admin", adminRouter)
-router.use("/space", spaceRouter)
 
 router.post("/signup", async (req, res) => {
     console.log("inside signup")
@@ -29,14 +23,6 @@ router.post("/signup", async (req, res) => {
     const hashedPassword = await hash(parsedData.data.password)
 
     try {
-        const exhist = await client.user.findUnique({
-            where:{
-                username:parsedData.data.username
-            }
-        })
-        if (exhist) {
-            res.status(400).json({message :"User already exhit"})
-        }
          const user = await client.user.create({
             data: {
                 username: parsedData.data.username,
@@ -44,10 +30,9 @@ router.post("/signup", async (req, res) => {
                 role: parsedData.data.type === "admin" ? "Admin" : "User",
             }
         })
-        res.status(200).json({
+        res.json({
             userId: user.id
         })
-        return
     } catch(e) {
         console.log("erroer thrown")
         console.log(e)
@@ -85,8 +70,7 @@ router.post("/signin", async (req, res) => {
             role: user.role
         }, JWT_SECRET);
 
-        res.status(200).json({
-            message:"signup success",
+        res.json({
             token
         })
     } catch(e) {
@@ -94,9 +78,27 @@ router.post("/signin", async (req, res) => {
     }
 })
 
-router.get('/elements', (req, res) => {
+router.get("/elements", async (req, res) => {
+    const elements = await client.element.findMany()
 
+    res.json({elements: elements.map(e => ({
+        id: e.id,
+        imageUrl: e.imageUrl,
+        width: e.width,
+        height: e.height,
+        static: e.static
+    }))})
 })
-router.get('/avatars', (req, res) => {
 
+router.get("/avatars", async (req, res) => {
+    const avatars = await client.avatar.findMany()
+    res.json({avatars: avatars.map(x => ({
+        id: x.id,
+        imageUrl: x.imageUrl,
+        name: x.name
+    }))})
 })
+
+router.use("/user", userRouter)
+router.use("/space", spaceRouter)
+router.use("/admin", adminRouter)
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,6 +1,6 @@
 const axios2 = require("axios");
 
-const BACKEND_URL = "http://127.0.0.1:3000"
+const BACKEND_URL = "http://localhost:3000"
 const WS_URL = "ws://localhost:3001"
 
 const axios = {
