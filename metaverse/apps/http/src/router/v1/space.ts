df22a423072bec2681061df6e993965f20b10090
Prasad Gurude <prasadgurude76@gmail.com>
2025-02-02 16:52:03 +0530

    startin with the websockt server building in ws folder

--- metaverse/apps/http/src/router/v1/space.ts
+++ metaverse/apps/http/src/router/v1/space.ts
@@ -66,7 +66,6 @@ spaceRouter.post("/", userMiddleware, async (req, res) => {
 
 
 spaceRouter.delete("/element", userMiddleware, async (req, res) => {
-    console.log("spaceElement?.space1 ")
     const parsedData = DeleteElementSchema.safeParse(req.body)
     if (!parsedData.success) {
         res.status(400).json({message: "Validation failed"})
@@ -80,7 +79,7 @@ spaceRouter.delete("/element", userMiddleware, async (req, res) => {
             space: true
         }
     })
-    console.log(spaceElement?.space)
+
     if (!spaceElement?.space.creatorId || spaceElement.space.creatorId !== req.userId) {
         res.status(403).json({message: "Unauthorized"})
         return
