464a406ef09f0961e003f148b9e13e798c99d13d
Prasad Gurude <prasadgurude76@gmail.com>
2025-02-04 21:13:31 +0530

    Backend is almost done

--- metaverse/apps/ws/src/RoomManager.ts
+++ metaverse/apps/ws/src/RoomManager.ts
@@ -1,11 +1,12 @@
-import { User } from "./User";
+import type { User } from "./User";
+import { OutgoingMessage } from "./types";
 
 export class RoomManager {
     rooms: Map<string, User[]> = new Map();
     static instance: RoomManager;
 
     private constructor() {
-        this.rooms = new Map()
+        this.rooms = new Map();
     }
 
     static getInstance() {
@@ -15,11 +16,29 @@ export class RoomManager {
         return this.instance;
     }
 
-    public addUser(spaceId, userId: User) {
+    public removeUser(user: User, spaceId: string) {
         if (!this.rooms.has(spaceId)) {
-            this.rooms.get(spaceId, [userId]);
-            return
+            return;
         }
-        this.rooms.set(spaceId, [...this.rooms.get(spaceId), userId])
+        this.rooms.set(spaceId, (this.rooms.get(spaceId)?.filter((u) => u.id !== user.id) ?? []));
+    }
+
+    public addUser(spaceId: string, user: User) {
+        if (!this.rooms.has(spaceId)) {
+            this.rooms.set(spaceId, [user]);
+            return;
+        }
+        this.rooms.set(spaceId, [...(this.rooms.get(spaceId) ?? []), user]);
+    }
+
+    public broadcast(message: OutgoingMessage, user: User, roomId: string) {
+        if (!this.rooms.has(roomId)) {
+            return;
+        }
+        this.rooms.get(roomId)?.forEach((u) => {
+            if (u.id !== user.id) {
+                u.send(message);
+            }
+        });
     }
 }
